44636ce8ecd12d84abefd2b184a828ddb768c74b
Joao Soares <joaosoaresdev@gmail.com>
2023-03-24 22:46:34 +0800

    feat(FAQ): starting FAQ page (not finished)

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export * from "./about";
 export * from "./home";
 export * from "./layout";
 export * from "./services";
+export * from "./shared";
--- src/pages/faq/index.tsx
+++ src/pages/faq/index.tsx
@@ -1,21 +1,50 @@
 import { Box, Typography, useTheme } from "@mui/material";
 import "@fontsource/inter";
+import {
+  AboutPresentation,
+  AssociatedBodies,
+  Specialties,
+  Values,
+} from "@/components";
+import { motion } from "framer-motion";
 
 export const FAQ = () => {
   const theme = useTheme();
 
   return (
-    <Box
-      display={"flex"}
-      alignItems={"center"}
-      justifyContent={"center"}
-      flexDirection={"column"}
-      width={"100%"}
-      textAlign={"center"}
-      py={10}
-      gap={8}
+    <motion.main
+      className="main__container"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 1.5 }}
     >
-      <Typography variant={"h1"}>Welcome to FAQ</Typography>
-    </Box>
+      <Box
+        display={"flex"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        flexDirection={"column"}
+        width={"100%"}
+        textAlign={"center"}
+        py={10}
+        gap={18}
+      >
+        <Box
+          display={"flex"}
+          flexDirection={"column"}
+          alignItems={"center"}
+          gap={7}
+        >
+          <Typography variant={"h1"}>FAQ</Typography>
+
+          <Typography variant={"body1"} fontWeight={450}>
+            Finding support can sometimes feel like a heavy burden.
+            <br />
+            So we’ve made it easier by creating a list of questions that we
+            think may help you.
+          </Typography>
+        </Box>
+      </Box>
+    </motion.main>
   );
 };
