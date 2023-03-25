8445d08e8cd21b83bcacb500f0cfddc2dc2fcfca
Joao Soares <joaosoaresdev@gmail.com>
2023-03-25 15:18:10 +0800

    feat(DropdownComponent): creating a shared component out of the dropdown list for reuse in multiple parts of project.

---
+++ src/components/shared/dropdown_item/index.tsx
@@ -0,0 +1,42 @@
+import {
+  Box,
+  Collapse,
+  Divider,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import RemoveIcon from "@mui/icons-material/Remove";
+import AddIcon from "@mui/icons-material/Add";
+
+interface IProps {
+  title: string;
+  isOpen: boolean;
+  setOpen: (value: boolean) => void;
+  collapsableContent: JSX.Element | JSX.Element[];
+}
+
+export const DropdownItem = ({
+  title,
+  isOpen,
+  setOpen,
+  collapsableContent,
+}: IProps) => {
+  return (
+    <>
+      <ListItemButton onClick={() => setOpen(!isOpen)}>
+        <ListItemText primary={title} />
+        <ListItemIcon>{isOpen ? <RemoveIcon /> : <AddIcon />}</ListItemIcon>
+      </ListItemButton>
+
+      <Collapse in={isOpen} timeout="auto" unmountOnExit>
+        {collapsableContent}
+      </Collapse>
+
+      <Divider />
+    </>
+  );
+};
--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,1 +1,2 @@
+export * from "./dropdown_item";
 export * from "./just_ask";
